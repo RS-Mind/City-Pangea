f91bad617214320758bc5f03d489b150d8ca57ce
RS_Mind <jared@awoodfamily.com>
2021-02-27 10:21:44 -0800

    Added pregame render

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -23,11 +23,12 @@ var overviewerConfig = {
         "LOWERRIGHT": 2
     },
     "worlds": [
+        "Terrain - overworld",
         "Pre-Game - overworld"
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1614030453",
+        "cacheTag": "1614432497",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -45,11 +46,57 @@ var overviewerConfig = {
             "zoomLevels": 7,
             "defaultZoom": 1,
             "maxZoom": 7,
+            "path": "Terrain North",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Terrain - overworld",
+            "last_rendertime": 1613094040,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                96,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "South",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "Terrain South",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Terrain - overworld",
+            "last_rendertime": 1613094040,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                96,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "North",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
             "path": "Pre North",
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Pre-Game - overworld",
-            "last_rendertime": 1613094040,
+            "last_rendertime": 1614410847,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
@@ -72,7 +119,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "Pre-Game - overworld",
-            "last_rendertime": 1613094040,
+            "last_rendertime": 1614410847,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
