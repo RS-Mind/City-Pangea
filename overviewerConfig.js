e9ac3cfb46645028ad8b23d05a4b22db328f22f6
RS_Mind <jared@awoodfamily.com>
2021-03-05 08:12:01 -0800

    Added Tribal Render

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -24,11 +24,12 @@ var overviewerConfig = {
     },
     "worlds": [
         "Terrain - overworld",
-        "Pre-Game - overworld"
+        "Pre-Game - overworld",
+        "Tribal - overworld"
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1614432497",
+        "cacheTag": "1614923149",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -120,6 +121,52 @@ var overviewerConfig = {
             "bgcolor": "#1a1a1a",
             "world": "Pre-Game - overworld",
             "last_rendertime": 1614410847,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                96,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "North",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "Tribal North",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Tribal - overworld",
+            "last_rendertime": 1614643244,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -1,
+                96,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "South",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "Tribal South",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Tribal - overworld",
+            "last_rendertime": 1614643244,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
